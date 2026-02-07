ee9cdb87006737e78f4c60dd90a6e7a5b16242fe
titir95biplab-dotcom <titir95biplab@gmail.com>
2026-02-07 07:02:47 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -30,7 +30,7 @@ messaging.onBackgroundMessage((payload) => {
 
 
 // --- STANDARD PWA CACHING (Keep your existing logic) ---
-const CACHE_NAME = 'jt-pos-v21';
+const CACHE_NAME = 'jt-pos-v22';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
